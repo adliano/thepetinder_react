dbd68983cc5e6c50fb80300c7aa7acb9fe77ca10
Adliano Alves <adliano@me.com>
2019-07-10 00:59:25 -0700

    first commit

---
+++ client/build/precache-manifest.7e9ce98d79c7a81e5a32fa20738032dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "673d178618d9c7987955",
+    "url": "/static/css/main.393e781e.chunk.css"
+  },
+  {
+    "revision": "673d178618d9c7987955",
+    "url": "/static/js/main.6bf694c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "2b83014a1cef748247fd8c8972155539",
+    "url": "/index.html"
+  }
+];
