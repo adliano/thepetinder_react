32ef1a6a001967a8b7ca5bebe4a66f409a74864a
Little Milo <no-reply>
2019-07-14 16:34:00 -0700

    added pages file, re-organized per class activity wk20 with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-import PetNavBar from './components/PetNavBar'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Splash from './pages/Splash';
+import shelterLogin from './pages/Shelter_Login';
+import shelterRegister from './pages/Shelter_Register';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-      <PetNavBar/>
-      </>
-    );
-  }
+import Navbar from './components/Navbar';
+import Wrapper from './components/Wrapper';
+import Footer from './components/Footer';
+
+function App () {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path='/' component={Splash} />
+          <Route exact path='/Shelter_Login' component={shelterLogin} />
+          <Route exact path='/Shelter_Register' component={shelterRegister} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
