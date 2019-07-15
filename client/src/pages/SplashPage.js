13e3574bc2a592db270310800b3604d52ffb57db
Adliano Alves <adliano@me.com>
2019-07-14 17:29:48 -0700

    spash page ready for use

--- client/src/pages/SplashPage.js
+++ client/src/pages/SplashPage.js
@@ -4,35 +4,46 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
-
 // Use Application Components
 import PetNavbar from '../components/PetNavBar'
 import PetFooter from '../components/PetFooter'
 import PetinderLogo from '../components/PetinderLogo'
 
+/**
+ * Page used for splash, it will have two buttons to 
+ * route user || shelter to espesific page
+ */
 class SplashPage extends Component {
     /**
      * onButtonClick()
      * Event listener used for buttons
      */
     onButtonClick = (event) => {
-        //Place logic here later
+        
         let _element = event.target
         
-        console.log(_element)
-        
+        if(_element.textContent === 'User'){
+            console.log('send user to user home page')
+            //Place logic here later
+        }
+        else{
+            console.log('send Shelter to shelter home page')
+            //Place logic here later
+        }
     }
   /**
+   * 
    * Render
+   * 
    */
   render () {
     return (
       <>
         <PetNavbar />
         <PetinderLogo />
-        <Container className='my-2 p-1 text-center'>
+        <Container className='my-2 p-2 text-center'>
           <Row>
-            <h1>
+            <h1 className='p-2'>
               Lorem cillum labore ex irure nostrud reprehenderit est sit
               incididunt aute anim ea voluptate.
             </h1>
@@ -49,26 +60,15 @@ class SplashPage extends Component {
               adipisicing consequat non elit. Mollit eiusmod commodo do magna
               aute Lorem fugiat ut irure Lorem consequat sunt magna ex. Dolore
               esse consectetur commodo voluptate ullamco non veniam minim cillum
-              aliqua. Laboris amet tempor tempor occaecat laborum. Labore in
-              magna amet nostrud velit laborum aliqua aliquip ipsum eiusmod
-              ullamco laboris esse. Pariatur laboris ex deserunt veniam aliqua
-              culpa ad labore aute. Lorem excepteur aliquip voluptate culpa
-              officia amet tempor dolore ex in. Ullamco aliqua non excepteur
-              minim velit. Veniam proident et aliqua cillum minim commodo anim
-              aute. Qui consequat do elit sunt officia pariatur nulla. Ipsum
-              esse Lorem anim nulla esse enim quis laborum adipisicing laborum.
-              Aliquip ad anim labore Lorem excepteur elit aliqua ex adipisicing
-              sit Lorem ad sunt. Veniam fugiat in qui ullamco ut aute non
-              aliquip. Officia occaecat reprehenderit aliqua qui laborum magna
-              veniam eiusmod non exercitation amet duis.
+              aliqua.
             </p>
           </Row>
-          <Row className='my-2 p-3'>
-              <Col>
-              <Button className='w-25' variant='primary'>User</Button>
+          <Row className='m-3 p-2'>
+              <Col md>
+              <Button className='w-25 m-2' variant='primary' onClick={this.onButtonClick}>User</Button>
               </Col>
-              <Col>
-              <Button className='w-25' variant='primary'>Shelter</Button>
+              <Col md>
+              <Button className='w-25 m-2' variant='primary' onClick={this.onButtonClick}>Shelter</Button>
               </Col>
           </Row>
         </Container>
