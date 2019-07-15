e4469d106ce5f27bd93d14c6575e024548ca2e4c
Little Milo <no-reply>
2019-07-14 18:49:36 -0700

    changed file names and set files to js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 // Import Pages
 // import SplashPage from './pages/SplashPage'
-import ShelterLogin from './pages/Shelter_Login'
-// import ShelterRegister from './pages/Shelter_Register'
+import ShelterLogin from './pages/ShelterLogin'
+// import ShelterRegister from './pages/ShelterRegister'
 
 // import logo from "./logo.svg";
 // import "./App.css";
@@ -23,3 +23,31 @@ class App extends Component {
 }
 
 export default App
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Splash from './pages/Splash';
+// import shelterLogin from './pages/Shelter_Login';
+// import shelterRegister from './pages/Shelter_Register';
+
+// import Navbar from './components/PetNavBar';
+// import Wrapper from './components/Wrapper';
+// // import Footer from './components/Footer';
+
+// function App () {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Wrapper>
+//           <Route exact path='/' component={Splash} />
+//           <Route exact path='/Shelter_Login' component={shelterLogin} />
+//           <Route exact path='/Shelter_Register' component={shelterRegister} />
+//         </Wrapper>
+//         {/* <Footer /> */}
+//       </div>
+//     </Router>
+//   )
+// }
+
+// export default App
