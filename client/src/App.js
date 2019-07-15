35e4cec65ee6a33ca251080c7e37429e7fde3d00
Little Milo <no-reply>
2019-07-14 19:16:09 -0700

    working on Login page now

--- client/src/App.js
+++ client/src/App.js
@@ -15,39 +15,11 @@ class App extends Component {
     return (
       <>
         {/* <SplashPage /> */}
-        {/* <ShelterLogin /> */}
-        <ShelterRegister />
+        <ShelterLogin />
+        {/* <ShelterRegister /> */}
       </>
     )
   }
 }
 
 export default App
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Splash from './pages/Splash';
-// import shelterLogin from './pages/Shelter_Login';
-// import shelterRegister from './pages/Shelter_Register';
-
-// import Navbar from './components/PetNavBar';
-// import Wrapper from './components/Wrapper';
-// // import Footer from './components/Footer';
-
-// function App () {
-//   return (
-//     <Router>
-//       <div>
-//         <Navbar />
-//         <Wrapper>
-//           <Route exact path='/' component={Splash} />
-//           <Route exact path='/Shelter_Login' component={shelterLogin} />
-//           <Route exact path='/Shelter_Register' component={shelterRegister} />
-//         </Wrapper>
-//         {/* <Footer /> */}
-//       </div>
-//     </Router>
-//   )
-// }
-
-// export default App
