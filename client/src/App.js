fa505097aaa316bf354c9d7ced55fdde80581780
karilowell <karinalowell@gmail.com>
2019-07-16 20:53:30 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 // Import Pages
-// import SplashPage from './pages/SplashPage'
+import SplashPage from './pages/SplashPage'
 // import ShelterLogin from './pages/Shelter_Login'
-import ShelterRegister from './pages/Shelter_Register'
+// import ShelterRegister from './pages/Shelter_Register'
 
 // import logo from "./logo.svg";
 // import "./App.css";
@@ -14,9 +14,9 @@ class App extends Component {
   render () {
     return (
       <>
-        {/* <SplashPage /> */}
+        <SplashPage />
         {/* <ShelterLogin /> */}
-        <ShelterRegister />
+        {/* <ShelterRegister /> */}
       </>
     )
   }
