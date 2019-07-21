9a5ce0f137bb00053cc3680db7ffd4610b2ab529
Adliano Alves <adliano@me.com>
2019-07-21 02:27:21 -0700

    added client/src/utils/API.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,17 @@
+export default {
+    // Gets all books
+    getPets: function() {
+      return fetch("/api/pet");
+    },
+    // Saves a book to the database
+    savePet: function(petData) {
+      return fetch("/api/pet", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(petData)
+      })
+    }
+  };
+  
