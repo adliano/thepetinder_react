9b7931f1c4f3624a27802542585d30b77c012b08
Adliano Alves <adliano@me.com>
2019-07-31 08:37:07 -0700

    changed and clear the AvailablePets page

--- client/src/pages/AvaliablePetsPage.js
+++ client/src/pages/AvaliablePetsPage.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react'
-// Use Bootstrap Components
 import Container from 'react-bootstrap/Container'
 
-/*import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-*/
-
-// Use Application Components
-import PetNavbar from '../components/PetNavBar'
-import PetFooter from '../components/PetFooter'
-import PetinderLogo from '../components/PetinderLogo'
 import PetCard from '../components/PetCard'
-
 
 /**
  * Page used for splash, it will have two buttons to
@@ -22,45 +11,25 @@ class AvaliablePetsPage extends Component {
 state= {
   pets: []
 }
-
+/**
+ * gettingData()
+ */
 gettingData = () => {fetch('/api/findAll')
   .then(response => response.json())
   .then (data => this.setState({pets:data},()=>console.log(this.state)))
-  /*.then(function (data) {
-    for (var i = 0; i < data.length; i++) {
-      console.log(data[i].petName)
-    }
-  })*/}  
+  }
+  /**
+   * componentDidMount()
+   */
   componentDidMount () {
     this.gettingData()
   }
-  
-  /**
-     * onButtonClick()
-     * Event listener used for buttons
-     */
-  /** *onButtonClick = (event) => {
-
-        let _element = event.target
-
-        if(_element.textContent === 'User'){
-            console.log('send user to user home page')
-            //Place logic here later
-        }
-        else{
-            console.log('send Shelter to shelter home page')
-            //Place logic here later
-        }
-    }
-    */
   /**
    *
    * Render
    *
    */
   renderPets = petsArray => {
-    // Some books doesn't have `imageLinks` available
-    // Therefor default img was set to void application to crash
     let _petsElements = petsArray.map(petInfo => {
       // Destructing object
       const {
@@ -71,8 +40,8 @@ gettingData = () => {fetch('/api/findAll')
         imgPath
       } = petInfo
       console.log(petInfo)
-      // each Book available in the JSON will be
-      // Added as <Book> element in _booksElements array
+      // each Pet available in the JSON will be
+      // Added as <PetCard> element in _booksElements array
       return (
         <PetCard
           key={id}
@@ -84,26 +53,16 @@ gettingData = () => {fetch('/api/findAll')
         />
       )
     })
-
-    // Return Array of <Book> elements
+    // Return Array of <PetCard> elements
     return _petsElements
   }  
-
-
   render () {
-    /*this.gettingData()
-    *this.componentDidMount()
-    */
-
-    
     return (
       <>
-        <PetNavbar />
-        <PetinderLogo />
+      <h1 className='text-center my-5'>Available Pets</h1>
         <Container className='my-2 p-2 container'>
           {this.renderPets(this.state.pets)}
         </Container>
-        <PetFooter />
       </>
     )
   }
