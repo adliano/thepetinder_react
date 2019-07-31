2007c837632c62c42dc1c1db0b6b7a9e7a262d5e
LuisERivas <lucho.rivas.villacorta@gmail.com>
2019-07-30 18:50:03 -0700

    added links for images

--- seeds/petsSeed.js
+++ seeds/petsSeed.js
@@ -4,43 +4,43 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('pets_tb').insert([{
-          name: 'abc',
-          type: 'abd',
-          attitude: 'abd',
-          isAdopted: true,
-          age: 11,
-          imgPath: 'dkdkdkd'
-        },
-        {
-          name: 'ddd',
-          type: 'abd',
-          attitude: 'abd',
-          isAdopted: true,
-          age: 11,
-          imgPath: 'dkdkdkd'
-        },
-        {
-          name: 'ccc',
-          type: 'abd',
-          attitude: 'abd',
-          age: 11,
-          imgPath: 'dkdkdkd'
-        },
-        {
-          name: 'ggg',
-          type: 'abd',
-          attitude: 'abd',
-          isAdopted: true,
-          age: 11,
-          imgPath: 'dkdkdkd'
-        },
-        {
-          name: 'fff',
-          type: 'abd',
-          attitude: 'abd',
-          age: 11,
-          imgPath: 'dkdkdkd'
-        }
+        name: 'abc',
+        type: 'abd',
+        attitude: 'abd',
+        isAdopted: true,
+        age: 11,
+        imgPath: 'https://5.imimg.com/data5/NQ/PJ/MY-37341530/beagle-dog-puppy-250x250.jpg'
+      },
+      {
+        name: 'ddd',
+        type: 'abd',
+        attitude: 'abd',
+        isAdopted: true,
+        age: 11,
+        imgPath: 'https://data.whicdn.com/images/302795711/large.jpg'
+      },
+      {
+        name: 'ccc',
+        type: 'abd',
+        attitude: 'abd',
+        age: 11,
+        imgPath: 'https://www.k9web.com/wp-content/uploads/2019/03/Pocket-Pitbull-3.jpg'
+      },
+      {
+        name: 'ggg',
+        type: 'abd',
+        attitude: 'abd',
+        isAdopted: true,
+        age: 11,
+        imgPath: 'https://mymodernmet.com/wp/wp-content/uploads/2019/07/russian-blue-cats-17.jpg'
+      },
+      {
+        name: 'fff',
+        type: 'abd',
+        attitude: 'abd',
+        age: 11,
+        imgPath: 'https://opimedia.azureedge.net/-/media/images/grt/editorial/articles/magazine-articles/2016/07-01/bald-eagles-and-golden-eagles/bald-eagle-square-jpg.jpg'
+      }
       ])
     })
 }
